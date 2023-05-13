14b4addbc3891cdca0f32fd807a86b1020a58286
DavitMelkonyan888 <davit.melkonyan888@gmail.com>
2023-05-13 12:43:47 +0400

    Skeleton

---
+++ src/main/java/com/example/airportspring/AirportspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.airportspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportspringApplication {
+    
+    public static void main (String[] args) {
+        SpringApplication.run(AirportspringApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/example/airportspring/AirportspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airportspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportspringApplicationTests {
+    
+    @Test
+    void contextLoads () {
+    }
+    
+}
